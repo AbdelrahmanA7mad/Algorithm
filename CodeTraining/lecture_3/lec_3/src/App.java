ccabf1223e61983361f286543d5f681651b3ed70
AbdelrahmanA7mad <abdelra7man.ahmad@gmail.com>
2025-03-09 16:00:01 +0200

    add second sort insertion function

--- CodeTraining/lecture_3/lec_3/src/App.java
+++ CodeTraining/lecture_3/lec_3/src/App.java
@@ -1,6 +1,31 @@
+/*
+ * Insertion Sort
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
         int arr[] = { 32,15,2,17,19,26,41,17,17};
+        // first_method(arr);
+        second_method(arr);
+
+    }
+    public static void second_method(int arr[]){
+        for (int i=1 ; i<arr.length;i++){
+            int key = arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j]>key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        for (int i = 0 ;i<arr.length-1;i++){
+            System.out.println(arr[i]);
+         }
+
+    }
+    
+    public static void first_method(int arr[]){
         
         int temp =0;
         for(int j=0;j<arr.length-1;j++){
@@ -15,6 +40,5 @@ public class App {
         for (int i = 0 ;i<arr.length-1;i++){
            System.out.println(arr[i]);
         }
-
     }
 }
